518c207a3835a1678f29ed7173ccb371011d05e6
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-04-29 13:40:01 +0100

    added user model

---
+++ core/user/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, ):
+        pass
+
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=256, blank=False)
+    last_name = models.CharField(max_length=256, blank=False)
+    email = models.EmailField(blank=False, max_length=256, unique=True)
+    username = None
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+    object = BaseUserManager()
+
+    def __str__(self):
+        return self.email

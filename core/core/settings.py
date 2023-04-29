50c4d8f37342b373e7334e34312ed0beb0c31d1d
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-04-29 23:11:05 +0100

    udpated postgres port

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,7 @@ DATABASES = {
         'NAME': 'core',
         'USER': 'sanju',
         'PASSWORD': 'Dhanush97',
-        'HOST': 'postgres',
+        'HOST': os.environ.get("HOST", "localhost"),
         'PORT': '5432',
     }
 }

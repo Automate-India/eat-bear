797037724995e5985252def9b45b7288884d6960
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-04-29 22:26:30 +0100

    added new test

--- core/user/tests.py
+++ core/user/tests.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework.status import HTTP_201_CREATED
 # Create your tests here.
+
+User = get_user_model()
 
 class TestUser(APITestCase):
 
@@ -14,3 +17,9 @@ class TestUser(APITestCase):
         url = reverse("signup")
         response = self.client.post(url, data={"first_name": first_name, "last_name": last_name, "email": email, "password": password}, format='json')
         self.assertEqual(response.status_code, HTTP_201_CREATED)
+        users = User.objects.all()
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].first_name, first_name)
+        self.assertEqual(users[0].last_name, last_name)
+        self.assertEqual(users[0].is_active, True)
+        self.assertEqual(users[0].is_superuser, False)

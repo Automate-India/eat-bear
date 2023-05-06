ef2a02488949761368bc17a9971ac60c95175225
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-05-06 17:51:24 +0100

    created new tests

--- ui/src/components/Input/Input.test.js
+++ ui/src/components/Input/Input.test.js
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import Input from "../Input/Input";
 
 describe("Input", () => {
@@ -9,8 +9,33 @@ describe("Input", () => {
 
         const inputNode = screen.getByLabelText(label)
 
-        expect(inputNode).toBeEnabled;
+        expect(inputNode).toBeEnabled();
 
         expect(inputNode).toHaveDisplayValue(inputValue);
+    });
+
+    it("should call the onChange function with updated input value", () => {
+        const inputValue = "dummyValue"
+        const label = "dummyLabel"
+        const dummyFunc = jest.fn(() => {});
+
+        render(<Input value={inputValue} label={label} onChange={dummyFunc}/>);
+        
+        const inputNode = screen.getByLabelText(label);
+        expect(inputNode).toHaveDisplayValue(inputValue);
+        
+        const updatedInputValue = "newInputValue";
+
+        fireEvent.change(inputNode, {target: {value: updatedInputValue}});
+        expect(dummyFunc.mock.calls).toHaveLength(1);
+    });
+
+    it("should be disabled if disabled is true", () => {
+        const inputValue = "dummyValue"
+        const label = "dummyLabel"
+        render(<Input value={inputValue} disabled label={label} />);
+
+        const inputNode = screen.getByLabelText(label);
+        expect(inputNode).toBeDisabled();
     })
 })

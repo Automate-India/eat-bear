2c94006c4680828786f6e7f34611cc729176abb4
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-05-06 16:02:24 +0100

    added onClick listener to the button

--- ui/src/components/Button/Button.js
+++ ui/src/components/Button/Button.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 import MuiButton from '@mui/material/Button';
 
 function Button(props) {
-    const {children, disabled, buttonType} = props;
+    const {children, disabled, buttonType, onClick} = props;
 
   return (
     <div>
-        <MuiButton variant={buttonType} disabled={disabled}>
+        <MuiButton  variant={buttonType} 
+                    disabled={disabled} 
+                    onClick={onClick}
+        >
             {children}
         </MuiButton>
     </div>
@@ -19,10 +22,12 @@ export default Button;
 Button.propTypes = {
     buttonType: PropTypes.oneOf(["text", "contained", "outlined"]),
     children: PropTypes.string.isRequired,
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func
 }
 
 Button.defaultProps = {
     buttonType: "contained",
-    disabled: false
+    disabled: false,
+    onClick: () => {}
 }
--- ui/src/components/Button/Button.test.js
+++ ui/src/components/Button/Button.test.js
@@ -1,6 +1,4 @@
-import { Experimental_CssVarsProvider } from "@mui/material";
-import { internal_processStyles } from "@mui/styled-engine";
-import {screen, render} from "@testing-library/react";
+import {screen, render, fireEvent} from "@testing-library/react";
 import Button from "../Button/Button";
 
 describe("Button", () => {
@@ -10,5 +8,14 @@ describe("Button", () => {
         const element = screen.getByRole("button");
         expect(element).toHaveTextContent(buttonText);
         expect(element).tobeEnabled;
+    })
+
+    it("should must call the function passed on clicking the button", async () => {
+        const buttonText = "Dummy Text";
+        const dummyFunc = jest.fn(() => {});
+        render(<Button onClick={dummyFunc}>{buttonText}</Button>);
+        const element = screen.getByRole("button");
+        fireEvent.click(element);
+        expect(dummyFunc.mock.calls).toHaveLength(1);
     })
 })

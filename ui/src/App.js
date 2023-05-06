65be9fd3d85eaf1978bc50279ed395c0be23b52f
Dhanush Thakkalapally <dhanushthakkalapally@gmail.com>
2023-05-06 23:24:17 +0100

    added files to navigate

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {Typography} from "@mui/material";
+import Signup from './public/pages/Signup/Signup';
 
 
 function App() {
   return (
+    <>
    <Typography variant='h3'> WelCome Bear! </Typography>
+   <Signup />
+   </>
   );
 }
 
--- ui/src/public/pages/Signup/Signup.test.js
+++ ui/src/public/pages/Signup/Signup.test.js
@@ -1,6 +1,5 @@
 import Signup from "../Signup/Signup";
 import {screen, render} from "@testing-library/react";
-import { Experimental_CssVarsProvider } from "@mui/material";
 
 describe("Signup", () => {
     it("should render first name field", () => {
@@ -19,13 +18,13 @@ describe("Signup", () => {
         render(<Signup />);
         const emailNode = screen.getByLabelText("Email");
         
-    })
+    });
 
     it("should render password field", () => {
         render(<Signup />);
         const passwordNode = screen.getByLabelText("Password");
         
-    })
+    });
 
     it("should render singup button", () => {
         render(<Signup />);
